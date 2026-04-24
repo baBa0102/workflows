3b0ab6b4d1e7766f4cf3007b2d2edc704dc2d356
Baeri <106418776+baBa0102@users.noreply.github.com>
2026-04-24 11:40:02 +0000

    Sync LeetCode submission Runtime - 1098 ms (7.01%), Memory - 55.6 MB (49.27%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,7 +1,28 @@
-import java.util.Arrays;
-class Solution{
-    public static int majorityElement(int[] nums){
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+//bruteforce approach 
+import java.util.*;
+public class Solution {
+    public static int majorityElement(int arr[]){
+        
+        int n= arr.length;
+        //using nested loops for iteration and counting
+        for(int i=0; i<= arr.length-1; i++){
+            int count = 1;
+            for (int j=i+1; j<arr.length ; j++){
+                if(arr[i] == arr[j]){
+                    count ++;
+                }
+            }
+            if(count > n/2){
+                return arr[i];
+            }
+        }
+        return -1;
     }
+    public static void main(String[] args) {
+        int arr[] = {3,3,4,2,4,4,2,4,4};
+        int result = majorityElement(arr);
+        System.out.println(result);
+    }
+    
 }
+
