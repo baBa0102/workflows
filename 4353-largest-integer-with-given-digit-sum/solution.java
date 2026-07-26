b0e7debc6c14d56f7707cae3d2e8dec3d2925af6
Baeri <106418776+baBa0102@users.noreply.github.com>
2026-07-26 13:47:28 +0000

    Sync LeetCode submission Runtime - 1 ms (49.70%), Memory - 42.5 MB (36.38%)

---
+++ 4353-largest-integer-with-given-digit-sum/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int largestInteger(int n, int s) {
+
+        if (s > 9 * n) {
+            return -1;
+        }
+
+        if (s == 0) {
+            return 0;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (n > 0) {
+            int digit = Math.min(9, s);
+            ans.append(digit);
+            s -= digit;
+            n--;
+        }
+
+        return Integer.parseInt(ans.toString());
+    }
+}
