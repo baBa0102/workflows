4b4f15a846b772e47047fe183f6e4d18b0fd8655
Baeri <106418776+baBa0102@users.noreply.github.com>
2026-08-03 14:07:35 +0000

    Sync LeetCode submission Runtime - 1 ms (99.95%), Memory - 94.4 MB (52.17%)

--- 0121-best-time-to-buy-and-sell-stock/solution.java
+++ 0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,22 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for(int i=1; i < prices.length; i++){
-            if(prices[i]<buy){
-                buy=prices[i];
-            }
-            else if(prices[i] - buy > profit){
-                profit = prices[i] - buy;
-            }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int sell = 0; sell < prices.length; sell++){
+            minPrice = Math.min(minPrice, prices[sell]);
+            maxProfit = Math.max(maxProfit, prices[sell]-minPrice);
         }
-        return profit;
-    }
-    public static void main (String[] args){
-        int prices[]={7,1,5,3,6,4};
-        Solution sol = new Solution();
-        int result = sol.maxProfit(prices);
-        System.out.println("Maximum Profit = " + result);
-
+        return maxProfit;
     }
 }
