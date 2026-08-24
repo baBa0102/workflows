5c87b9b4ee6e1f8fd5e39070420bff1fdee6866d
Baeri <106418776+baBa0102@users.noreply.github.com>
2026-08-24 12:04:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (47.62%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -1,36 +1,26 @@
-import java.util.Arrays;
-
-// The Solution class should be a top-level class
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;      // Pointer for end of valid nums1
-        int j = n - 1;      // Pointer for end of nums2
-        int k = m + n - 1;  // Pointer for end of total nums1 array
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+        int p1 = m-1;
+        int p2 = n-1;
+        int p = m+n-1;
+        while(p >=0){
+            if(p1 <0){
+                nums1[p]= nums2[p2];
+                p2--;
             }
+            else if(p2 <0){
+                nums1[p] = nums1[p1];
+                p1--;
+            }
+            else if (nums1[p1]> nums2[p2]){
+                nums1[p]= nums1[p1];
+                p1--;
+            }
+            else{
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
         }
-
-        // Only need to catch the remainder of nums2
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        
-        sol.merge(nums1, 3, nums2, 3);
-        
-        System.out.println(Arrays.toString(nums1));
     }
 }
