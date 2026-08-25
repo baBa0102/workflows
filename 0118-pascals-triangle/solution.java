b474da414f8157d2efe95117ea06807da080406e
Baeri <106418776+baBa0102@users.noreply.github.com>
2026-08-25 10:19:37 +0000

    Sync LeetCode submission Runtime - 1 ms (95.42%), Memory - 43.5 MB (53.21%)

---
+++ 0118-pascals-triangle/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows <= 0){
+            return result;
+        }
+        for (int i = 0; i<numRows; i++){
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j< i; j++){
+                List<Integer> prevRow = result.get(i-1);
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+            if(i>0){
+                row.add(1);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
